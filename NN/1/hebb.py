69ae073499b189f57c2e90109ba829a9aac7a76f
innerac <anjicun@live.com>
2015-10-12 19:27:43 +0800

    完成了自联想的注释和测试

--- NN/1/hebb.py
+++ NN/1/hebb.py
@@ -1,15 +1,19 @@
+#coding:utf-8
 import numpy as np
 import show
 hardlims = lambda x: 1 if x >= 0 else -1
 
-def training():
-	p0 = np.matrix([-1,1,1,1,-1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, 1,-1,-1,-1,1, -1,1,1,1,-1])
-	p1 = np.matrix([-1,1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1, -1,-1,1,-1,-1])
-	p2 = np.matrix([1,1,1,-1,-1, -1,-1,-1,1,-1, -1,-1,-1,1,-1, -1,1,1,-1,-1, -1,1,-1,-1,-1, -1,1,1,1,1])
-	W = p0.T * p0 + p1.T * p1 + p2.T * p2
-	
+# 开始训练，传入训练样本 list,返回权值矩阵
+def training(ps):
+	# 初始化权值矩阵 W
+	ze = np.zeros([30,30])
+	W = np.matrix(ze)
+	# 使用Hebb规则求权值矩阵
+	for p in ps:
+		pp = np.matrix(p)
+		W += pp.T * pp
 	return W
-
+# 开始测试，输入测试样例和权值矩阵,返回输出的矩阵并且转换为数组
 def start(pt,W):
 	pt = np.matrix(pt)
 	
--- NN/1/show.py
+++ NN/1/show.py
@@ -1,30 +1,30 @@
+#coding:utf-8
 import cv2.cv as cv
 
 colorw = (255,255,255)
 colorb = (0,0,0)
 image = 1
-def drawPoints(x,y):
-	radius = 1
-	circle_center = (250+x*10,250-y*10)
-	cv.Circle(image, circle_center, radius, colorw)
 
-def drawMatix(x0,y0):
+# 画白色格子,左上角坐标
+def drawCell(x0,y0):
 	rect_start = (x0,y0) 
 	x1 = x0+100
 	y1 = y0+100
 	rect_end = (x1,y1)
 	cv.Rectangle(image, rect_start, rect_end, colorw, -1, 0)
 
+# 画黑色直线,起始点坐标
 def drawLine(x0,y0,x1,y1):
 	cv.Line(image, (x0,y0), (x1,y1), colorb, 1, 8)
 
+# 画棋盘,给定画的条数
 def init(width,height):
 	cv.Rectangle(image, (500,0), (700,600), colorw, -1, 0)
 	for i in range(width):
 		drawLine(i*100,0,i*100,600)
 	for i in range(height):
 		drawLine(0,i*100,1200,i*100)
-
+# 画变化图像
 def drawTwo(p,a):
 	global image
 	width = 1200
@@ -41,13 +41,13 @@ def drawTwo(p,a):
 		if p[i] == -1:
 			x = i % 5
 			y = (i - x) / 5
-			drawMatix(x*100,y*100)
+			drawCell(x*100,y*100)
 	
 	for i in range(30):
 		if a[i] == -1:
 			x = i % 5
 			y = (i - x) / 5
-			drawMatix(700+x*100,y*100)
+			drawCell(700+x*100,y*100)
 	
 	init(13,7)
 	
